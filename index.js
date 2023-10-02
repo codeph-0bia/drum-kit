7234e7c3004a1edd65effdb363b3811d31809bda
codeph-0bia <codephobia642@gmail.com>
2023-10-02 18:53:19 +0530

    key press and click sounds added

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-//TODO: add event listeners for click to all buttons
-
 let drumArray = document.querySelectorAll(".drum");
 
-for (const d of drumArray) {
-    d.addEventListener("click", function() {
-        alert("clicked");
+for (const e of drumArray) {
+    e.addEventListener("click", function() {
+        let buttonPressed = this.innerHTML;
+        makeSound(buttonPressed);
     });
 }
 
-//TODO: key specific event listeners
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+})
+
+function makeSound(key) {
+    let drumSounds = {
+        "w": new Audio("sounds/crash.mp3"),
+        "a": new Audio("sounds/kick-bass.mp3"),
+        "s": new Audio("sounds/snare.mp3"),
+        "d": new Audio("sounds/tom-1.mp3"),
+        "j": new Audio("sounds/tom-2.mp3"),
+        "k": new Audio("sounds/tom-3.mp3"),
+        "l": new Audio("sounds/tom-4.mp3")
+    }
+
+    switch (key) {
+        case "w":
+        case "a":
+        case "s":
+        case "d":
+        case "j":
+        case "k":
+        case "l":
+            drumSounds[key].play();
+            break;
+    }
+}
